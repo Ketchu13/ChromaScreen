c96155ce7f4ea67f5c7228e03e78aa6903627f4e
ketchu13 <ketchu13@hotmail.com>
2023-11-22 16:57:28 +0100

    reformat/clean code and fix dialog issue

--- panels/co_print_language_select_screen.py
+++ panels/co_print_language_select_screen.py
@@ -22,35 +22,35 @@ class CoPrintSplashScreenPanel(ScreenPanel):
     def __init__(self, screen, title):
         super().__init__(screen, title)
 
-
-        #wd = os.getcwd()
-        #os.chdir("/home/noya/klipper/")
-        #make_process = subprocess.Popen(['make', 'menuconfig'], shell=False, stdout=subprocess.PIPE, cwd="/home/noya/klipper/", stderr=subprocess.STDOUT)
-        #make_process = subprocess.check_output(['make', 'menuconfig']).decode('utf -8')
-        #out, err = make_process.communicate()
-        #print("stdout: {}".format(out))
-        #os.chdir(wd)
-
-        self.dialog = None
+        self.dialog = Gtk.Dialog()
         self.selected = None
 
         languages = [
             {'Lang':'en' ,'Name': _('English'), 'Icon': 'English', 'Button': Gtk.RadioButton()},
-            {'Lang':'fr' ,'Name': _('French'), 'Icon': 'France', 'Button': Gtk.RadioButton()},
+            {'Lang':'fr' ,'Name': _('French') , 'Icon': 'France' , 'Button': Gtk.RadioButton()},
             {'Lang':'ge' ,'Name': _("Deutsch"), 'Icon': 'Germany', 'Button': Gtk.RadioButton()},
-            {'Lang':'tr' ,'Name': _("Turkish"), 'Icon': 'Turkey', 'Button': Gtk.RadioButton()},
-            {'Lang':'it' ,'Name': _('Italian'), 'Icon': 'Italy', 'Button': Gtk.RadioButton()},
-            {'Lang':'sp' ,'Name': _('Spanish'), 'Icon': 'Spain', 'Button': Gtk.RadioButton()},
+            {'Lang':'tr' ,'Name': _("Turkish"), 'Icon': 'Turkey' , 'Button': Gtk.RadioButton()},
+            {'Lang':'it' ,'Name': _('Italian'), 'Icon': 'Italy'  , 'Button': Gtk.RadioButton()},
+            {'Lang':'sp' ,'Name': _('Spanish'), 'Icon': 'Spain'  , 'Button': Gtk.RadioButton()},
         ]
         
-        self.labels['actions'] = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL)
-        self.labels['actions'].set_hexpand(True)
-        self.labels['actions'].set_vexpand(False)
-        self.labels['actions'].set_halign(Gtk.Align.CENTER)
-        self.labels['actions'].set_homogeneous(True)
+        self.labels['actions'] = Gtk.Box(
+            orientation=Gtk.Orientation.HORIZONTAL,
+            hexpand=True,
+            vexpand=False,
+            halign=Gtk.Align.CENTER,
+            homogeneous=True
+        )
         self.labels['actions'].set_size_request(self._gtk.content_width, -1)
-       
-        initHeader = InitHeader (self, _('Language Settings'), _('Please specify the system language'), "Geography")
+
+        group = None
+
+        initHeader = InitHeader(
+            self,
+            _('Language Settings'),
+            _('Please specify the system language'),
+            "Geography"
+        )
 
         '''diller'''
         grid = Gtk.Grid(
@@ -61,19 +61,30 @@ class CoPrintSplashScreenPanel(ScreenPanel):
         row = 0
         count = 0
 
-        group = languages[0]['Button']
-
         current_lang = self._config.current_lang
 
         for language in languages:
-            languageImage = self._gtk.Image(language['Icon'], self._gtk.content_width * .05 , self._gtk.content_height * .05)
+            languageImage = self._gtk.Image(language['Icon'], self._gtk.content_width * .05, self._gtk.content_height * .05)
+            languageName = Gtk.Label(language['Name'], name="language-label")
 
-            languageName = Gtk.Label(language['Name'],name ="language-label")
+            language['Button'] = Gtk.RadioButton.new_with_label_from_widget(group, "")
+            checked = current_lang == language['Lang']
+            if checked:
+                if not language['Button'].get_active():
+                    language['Button'].set_active(True)
+                    # group = language['Button']
+            else:
+                language['Button'].set_active(False)
+            #{if current_lang == language['Lang']:
+            #    language['Button'] = Gtk.RadioButton.new_with_label_from_widget(group, "")
 
-            language['Button'] = Gtk.RadioButton.new_with_label_from_widget(group,"")
-            if current_lang == language['Lang']:
-                language['Button'] = Gtk.RadioButton.new_with_label_from_widget(None,"")
+            """languageName = Gtk.Label(language['Name'], name="language-label")
 
+            language['Button'] = Gtk.RadioButton.new_with_label_from_widget(group, "")
+            # check the radio button if it is the current language and it is not already checked
+            if current_lang == language['Lang'] and not language['Button'].get_active():
+                language['Button'].set_active(True)
+            """
             language['Button'].connect("toggled", self.radioButtonSelected, language['Lang'])
 
             languageBox = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=50)
@@ -90,23 +101,26 @@ class CoPrintSplashScreenPanel(ScreenPanel):
             eventBox.add(languageBox)
 
             f.add(eventBox)
+
             grid.attach(f, count, row, 1, 1)
+            if group is None:
+                group = language['Button']
+
             count += 1
             if count % 2 == 0:
                 count = 0
                 row += 1
-        
+
         gridBox = Gtk.Box()
         gridBox.set_halign(Gtk.Align.CENTER)
         gridBox.add(grid)
-        '''diller bitis'''
+
         
         # TODO add an apply bouton, avoid apply on select
-        
-        self.continueButton = Gtk.Button(_('Continue'),name ="flat-button-blue")
-        self.continueButton.connect("clicked", self.on_click_continue_button)
-        
-        self.continueButton.set_hexpand(True)
+
+        self.continueButton = Gtk.Button(_('Continue'), name="flat-button-blue", hexpand=True)
+        self.continueButton.connect("clicked", self.on_click_continue_button, "co_print_contract_approval")
+
         buttonBox = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=0)
         buttonBox.pack_start(self.continueButton, False, False, 0)
        
@@ -115,16 +129,20 @@ class CoPrintSplashScreenPanel(ScreenPanel):
         main.pack_end(buttonBox, True, True, 20)
         main.pack_end(gridBox, True, True, 20)
         
-        self.show_restart_buttons()
-      
-        self.content.add(main)
-        self._screen.base_panel.visible_menu(False)
+        # self.show_restart_buttons()
 
-    def on_click_continue_button(self, continueButton):
-        self._screen.show_panel("co_print_contract_approval", "co_print_contract_approval", None, 2)
+        self.content.add(main)
+        # self._screen.base_panel.visible_menu(False)
+
+    def on_click_continue_button(self, continueButton, target_panel):
+        self._screen.show_panel(target_panel, target_panel, None, 2)
 
     def changeLang(self, lang):
+        if self._config.current_lang == lang:
+            return
+        # Apply the language change to KlipperScreen config
         self._screen.change_language(lang)
+
         lang_map = {
             'en': 'en_US.UTF-8',
             'tr': 'tr_TR.UTF-8',
@@ -145,20 +163,24 @@ class CoPrintSplashScreenPanel(ScreenPanel):
 
         locale_code = lang_map.get(lang, 'en_US.UTF-8')
         locale_code_language = language_map.get(lang, 'en_US')
-        self.open_info_dialog()
-        sudoPassword = 'c317tek'
-        command = 'locale-gen ' + locale_code_language
-        p = os.system('echo %s|sudo -S %s' % (sudoPassword, command))
+        current_system_lang = self.get_system_region()
+        # only apply if the system language is different from the selected language
+        if current_system_lang != locale_code:
+            # self.open_info_dialog()
+            sudoPassword = 'c317tek'
+            command = 'locale-gen ' + locale_code_language
+            p = os.system('echo %s|sudo -S %s' % (sudoPassword, command))
 
-        command2 = 'update-locale LANG=' + locale_code
-        p = os.system('echo %s|sudo -S %s' % (sudoPassword, command2))
+            command2 = 'update-locale LANG=' + locale_code
+            p = os.system('echo %s|sudo -S %s' % (sudoPassword, command2))
 
-        command3 = 'update-locale LC_ALL=' + locale_code
-        p = os.system('echo %s|sudo -S %s' % (sudoPassword, command3))
-        self.finished()
+            command3 = 'update-locale LC_ALL=' + locale_code
+            p = os.system('echo %s|sudo -S %s' % (sudoPassword, command3))
+            # self.close_dialog(self.dialog)
 
     def radioButtonSelected(self, button, lang):
-        self.changeLang(lang)
+        if button.get_active():
+            self.changeLang(lang)
     
     def eventBoxLanguage(self, button, gparam, lang):
         self.changeLang(lang)
@@ -171,88 +193,32 @@ class CoPrintSplashScreenPanel(ScreenPanel):
         ))
         return active
 
-    def update_text(self, text):
-        
-        self.show_restart_buttons()
-
-    def clear_action_bar(self):
-        for child in self.labels['actions'].get_children():
-            self.labels['actions'].remove(child)
-
-    def show_restart_buttons(self):
-        self.clear_action_bar()
-        if self.ks_printer_cfg is not None and self._screen._ws.connected:
-            power_devices = self.ks_printer_cfg.get("power_devices", "")
-            if power_devices and self._printer.get_power_devices():
-                logging.info(f"Associated power devices: {power_devices}")
-                self.add_power_button(power_devices)
-
     def open_info_dialog(self):
-        self.dialog = InfoDialog(self, _("Loading selected language;\nPlease wait.."), True)
+        self.dialog = InfoDialog(self, _("Loading selected language.\nPlease wait.."), True)
         self.dialog.get_style_context().add_class("alert-info-dialog")
       
         self.dialog.set_decorated(False)
         self.dialog.set_size_request(0, 0)
 
+        #timer_duration = 30000
+        #GLib.timeout_add(timer_duration, self.close_dialog, self.dialog)
         response = self.dialog.run()
+        return False
 
     def finished(self):
         self.dialog.response(Gtk.ResponseType.CANCEL)
         self.dialog.destroy()
+    def close_dialog(self, dialog):
+        dialog.response(Gtk.ResponseType.CANCEL)
+        dialog.destroy()
 
+    def get_system_region(self):
+        region = 'en_US'
+        try:
+            system_locale = locale.getlocale()
+            if system_locale[0]:
+                region = system_locale[0]
+        except Exception as e:
+            print("Error getting system locale: %s" % e)
 
-    def add_power_button(self, powerdevs):
-        self.labels['power'] = self._gtk.Button("shutdown", _("Power On Printer"), "color3")
-        self.labels['power'].connect("clicked", self._screen.power_devices, powerdevs, True)
-        self.check_power_status()
-        self.labels['actions'].add(self.labels['power'])
-
-    def activate(self):
-        self.check_power_status()
-        self._screen.base_panel.show_macro_shortcut(False)
-        self._screen.base_panel.show_heaters(False)
-        self._screen.base_panel.show_estop(False)
-
-    def check_power_status(self):
-        if 'power' in self.labels:
-            devices = self._printer.get_power_devices()
-            if devices is not None:
-                for device in devices:
-                    if self._printer.get_power_device_status(device) == "off":
-                        self.labels['power'].set_sensitive(True)
-                        break
-                    elif self._printer.get_power_device_status(device) == "on":
-                        self.labels['power'].set_sensitive(False)
-
-    def firmware_restart(self, widget):
-        self._screen._ws.klippy.restart_firmware()
-
-    def restart(self, widget):
-        self._screen._ws.klippy.restart()
-
-    def shutdown(self, widget):
-        if self._screen._ws.connected:
-            self._screen._confirm_send_action(widget,
-                                              _("Are you sure you wish to shutdown the system?"),
-                                              "machine.shutdown")
-        else:
-            logging.info("OS Shutdown")
-            os.system("systemctl poweroff")
-
-    def restart_system(self, widget):
-        if self._screen._ws.connected:
-            self._screen._confirm_send_action(widget,
-                                              _("Are you sure you wish to reboot the system?"),
-                                              "machine.reboot")
-        else:
-            logging.info("OS Reboot")
-            os.system("systemctl reboot")
-
-    def retry(self, widget):
-        self.update_text((_("Connecting to %s") % self._screen.connecting_to_printer))
-        if self._screen._ws and not self._screen._ws.connecting:
-            self._screen._ws.retry()
-        else:
-            self._screen.reinit_count = 0
-            self._screen.init_printer()
-        self.show_restart_buttons()
+        return region
