21c131d572b92dd7c32ce33f7197c42b40046f33
ketchu13 <ketchu13@hotmail.com>
2023-11-26 19:21:53 +0100

    code clean and fix missing '_'

--- panels/co_print_product_naming.py
+++ panels/co_print_product_naming.py
@@ -29,18 +29,18 @@ class CoPrintProductNaming(ScreenPanel):
         initHeader = InitHeader (self, _('Rename Your Device'),_('Please specify a custom name for your device.'), "naming")
 
         self.deviceImage = self._gtk.Image("device", self._gtk.content_width * .4 , self._gtk.content_height * .4)
-       
+
         self.continueButton = Gtk.Button(_('Continue'), name="flat-button-blue")
         self.continueButton.connect("clicked", self.on_click_continue_button)
         self.continueButton.set_hexpand(True)
         self.buttonBox = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=0)
         self.buttonBox.pack_start(self.continueButton, False, False, 0)
-        
+
         self.entry = Gtk.Entry(name="device-name")
         self.entry.connect("activate", self.rename)
         self.entry.connect("focus-in-event", self.give_name)
         self.entry.set_width_chars(5)
-        self.entry.set_placeholder_text(("Device Name"))
+        self.entry.set_placeholder_text(_("Device Name"))
         self.entry.set_margin_left(self._gtk.action_bar_width *3)
         self.entry.set_margin_right(self._gtk.action_bar_width *3)
         self.entry.set_text("ChromaPad")
@@ -48,9 +48,9 @@ class CoPrintProductNaming(ScreenPanel):
         eventBox = Gtk.EventBox()
         eventBox.connect("button-press-event", self.give_name)
         eventBox.add(self.entry)
-        
+
         backIcon = self._gtk.Image("back-arrow", 35, 35)
-        backLabel = Gtk.Label(_("Back"), name="bottom-menu-label")            
+        backLabel = Gtk.Label(_("Back"), name="bottom-menu-label")
         backButtonBox = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=0)
         backButtonBox.set_halign(Gtk.Align.CENTER)
         backButtonBox.set_valign(Gtk.Align.CENTER)
@@ -62,7 +62,7 @@ class CoPrintProductNaming(ScreenPanel):
         self.backButton.set_always_show_image(True)
         mainBackButtonBox = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=0)
         mainBackButtonBox.pack_start(self.backButton, False, False, 0)
-        
+
         self.tempBox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=0)
         self.tempBox.pack_start(self.deviceImage, False, False, 0)
         self.tempBox.pack_start(self.buttonBox, False, False, 0)
@@ -76,14 +76,14 @@ class CoPrintProductNaming(ScreenPanel):
         self.content.add(self.main)
 
     def give_name(self, a, b):
-       
+
         for child in self.tempBox.get_children():
-            self.tempBox.remove(child) 
+            self.tempBox.remove(child)
         self._screen.show_keyboard()
         self.content.show_all()
         box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
         box.set_size_request(self._screen.gtk.content_width, self._screen.gtk.keyboard_height)
-       
+
         box.get_style_context().add_class("keyboard_box")
         box.add(Keyboard(self._screen, self.remove_keyboard, entry=self.entry))
         self.tempBox.pack_end(box, False, False, 0)
@@ -91,7 +91,7 @@ class CoPrintProductNaming(ScreenPanel):
 
     def remove_keyboard(self, widget=None, event=None):
         for child in self.tempBox.get_children():
-            self.tempBox.remove(child) 
+            self.tempBox.remove(child)
         self.tempBox.pack_start(self.deviceImage, False, False, 0)
         self.tempBox.pack_start(self.buttonBox, False, False, 0)
         self.content.show_all()
@@ -109,7 +109,7 @@ class CoPrintProductNaming(ScreenPanel):
 
         # create user with password for sudo access
 
-        # password ="12345" 
+        # password ="12345"
         # encPass = crypt.crypt(password,"22")
 
         # command2 = "useradd -p "+encPass+" " + self.entry.get_text()
@@ -124,7 +124,6 @@ class CoPrintProductNaming(ScreenPanel):
 
         # go to Wi-Fi selection screen
         self._screen.show_panel("co_print_wifi_selection", "co_print_wifi_selection", None, 2)
-        
+
     def on_click_back_button(self, button, target_panel):
         self._screen.show_panel(target_panel, target_panel, None, 2)
-
