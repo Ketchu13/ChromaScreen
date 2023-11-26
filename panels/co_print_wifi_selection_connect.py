d64c82301dd3042e72e160b9b674c0aa0a912d51
ketchu13 <ketchu13@hotmail.com>
2023-11-26 19:52:50 +0100

    clean code before following "Add Wi-Fi networks detection display"

--- panels/co_print_wifi_selection_connect.py
+++ panels/co_print_wifi_selection_connect.py
@@ -19,18 +19,22 @@ class CoPrintWifiSelectionConnect(ScreenPanel):
 
     def __init__(self, screen, title):
         super().__init__(screen, title)
-        
-        image = self._gtk.Image("wifi-connected", self._gtk.content_width * .25 , self._gtk.content_height * .25)
+
+        self.password = None
+        self.selectedMenu = None
+
+
+        image = self._gtk.Image("wifi-connected", self._gtk.content_width * .25, self._gtk.content_height * .25)
         subTitle = Gtk.Label(_('Connection Completed'), name="init-header-subtitle-label")
-       
+
         selectBox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=50)
-       
+
         selectBox.pack_start(image, False, True, 10)
         selectBox.pack_end(subTitle, False, False, 10)
-        
-        
+
+
         # ComboBox'a öğeler ekle
-        
+
         self.selectedWifiImage = self._gtk.Image(
             "sinyal",
             self._gtk.content_width * .05,
@@ -41,16 +45,16 @@ class CoPrintWifiSelectionConnect(ScreenPanel):
         self.selectedWifiName.set_alignment(0, 0.5)
         self.selectedWifiImage.set_alignment(1, 0.5)
 
-        
+
         self.selectedWifiBox = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=0, name= 'wifi')
-    
+
         self.selectedWifiBox.pack_start(self.selectedWifiName, True, True, 5)
         self.selectedWifiBox.pack_end(self.selectedWifiImage, True, True, 15)
         self.selectedWifiBox.set_size_request(150, 70)
         self.selectedWifiBox.set_margin_left(self._gtk.action_bar_width *3)
         self.selectedWifiBox.set_margin_right(self._gtk.action_bar_width*3)
-        
-        self.continueButton = Gtk.Button(_('Continue'),name ="flat-button-blue")
+
+        self.continueButton = Gtk.Button(_('Continue'), name="flat-button-blue")
         self.continueButton.connect("clicked", self.on_click_continue_button)
         self.continueButton.set_hexpand(True)
         self.continueButton.set_margin_left(self._gtk.action_bar_width *4)
@@ -59,16 +63,16 @@ class CoPrintWifiSelectionConnect(ScreenPanel):
         buttonBox.pack_start(self.continueButton, False, True, 0)
         buttonBox.set_center_widget(self.continueButton)
 
-    
+
         self.main = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=0)
-       
+
         self.main.pack_start(selectBox, False, False, 0)
         self.main.pack_end(buttonBox, False, False, 20)
         self.main.pack_end(self.selectedWifiBox, False, True, 0)
-        
+
         self.content.add(self.main)
-       
-    def show_message_box(self,title,message):
+
+    def show_message_box(self, title, message):
         window = Gtk.Window(title)
         window.set_default_size(300, 150)
         window.set_position(Gtk.WindowPosition.CENTER_ALWAYS)
@@ -96,6 +100,6 @@ class CoPrintWifiSelectionConnect(ScreenPanel):
         self.password = password
         self.selectedWifiName.set_label(self.selectedMenu)
         self.selectedPassword.set_label(self.password)
-        
+
     def on_click_continue_button(self, continueButton):
         self._screen.show_panel("co_print_fwmenu_selection", "co_print_fwmenu_selection", None, 2)
